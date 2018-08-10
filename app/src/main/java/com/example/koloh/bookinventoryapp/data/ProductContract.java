92dec73c7c1dabd4e401a17fdb8d72cf97d98635
Esther Weyinmi Koloh <kolohw@gmail.com>
2018-08-10 16:19:32 +0200

    Initial commit - stage 2-productContract class updated

--- app/src/main/java/com/example/koloh/bookinventoryapp/data/ProductContract.java
+++ app/src/main/java/com/example/koloh/bookinventoryapp/data/ProductContract.java
@@ -1,6 +1,8 @@
 package com.example.koloh.bookinventoryapp.data;
 
 
+import android.content.ContentResolver;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 //API contract for the Inventory App of Book Products
@@ -10,11 +12,30 @@ public final class ProductContract {
     private ProductContract() {
     }
 
+    //Name for the content provider
+    public static final String CONTENT_AUTHORITY = "com.example.koloh.bookinventoryapp";
+
+    //Base for all URI's for contacting the content provider
+    public static final Uri BASE_CONTENT_URI = Uri.parse ( "content://" + CONTENT_AUTHORITY );
+
+    //Possible path for the uri: content://com.example.koloh.bookinventoryapp/books
+    public static final String PATH_BOOKS = "books";
+
     /*
      * Inner class that defines constant values for the database table of the Inventory App
      * Each entry in the table represents a book product
      */
     public static final class BookEntry implements BaseColumns {
+
+        //The content URI to access the book data in the provider
+        public static final Uri CONTENT_URI = Uri.withAppendedPath ( BASE_CONTENT_URI, PATH_BOOKS );
+
+        //THE MIME type of the CONTENT_URI for a list of books
+        public static final String CONTENT_LIST_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+        //The MIME type of the CONTENT_URI for a single book
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
 
         //Name of the database table for books
         public final static String TABLE_NAME = "books";
